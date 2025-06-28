3948200b2c4ba3feda44e21e509d3fa64dc7a2fa
Ayush Singh <140960236+Ayush6108@users.noreply.github.com>
2025-06-28 23:30:13 +0530

    Adding Junit5 HandsOn

---
+++ Week 2 - 6376485/JUnit /Exercise 4/Code/CalculatorTest.java
@@ -0,0 +1,38 @@
+package junitdemo2;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class CalculatorTest {
+	private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        // Arrange
+        calculator = new Calculator();
+        System.out.println("Before each test");
+    }
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("After each test");
+    }
+
+    @Test
+    public void testAddition() {
+        // Act
+        int result = calculator.add(2, 3);
+
+        // Assert
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Act
+        int result = calculator.subtract(10, 4);
+
+        // Assert
+        assertEquals(6, result);
+    }
+}
