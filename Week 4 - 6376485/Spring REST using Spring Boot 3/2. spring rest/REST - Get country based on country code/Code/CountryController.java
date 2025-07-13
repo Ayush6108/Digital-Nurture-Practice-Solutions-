531d6fe0ab5d64b59bf0bfe257757cbc2f985a4a
Ayush Singh <singhayush61092050@gmail.com>
2025-07-13 17:26:11 +0530

    Week 4 Hands on

---
+++ Week 4 - 6376485/Spring REST using Spring Boot 3/2. spring rest/REST - Get country based on country code/Code/CountryController.java
@@ -0,0 +1,38 @@
+package com.cognizant.spring_learn1.controller;
+
+import com.cognizant.spring_learn1.model.Country;
+import com.cognizant.spring_learn1.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    // ✅ REST - Country Web Service
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        LOGGER.info("START - getCountryIndia()");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("in", Country.class);
+        LOGGER.info("END - getCountryIndia()");
+        return country;
+    }
+
+    // ✅ REST - Get country based on country code
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START - getCountry() with code: {}", code);
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END - getCountry()");
+        return country;
+    }
+}
